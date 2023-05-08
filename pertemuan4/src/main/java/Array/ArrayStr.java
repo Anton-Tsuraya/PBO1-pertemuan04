254d67055fe355363e1f46948c8c1fb1d28a87d4
Anton <129922565+Anton-Tsuraya@users.noreply.github.com>
2023-05-08 20:02:00 +0800

    Create ArrayStr.java

---
+++ pertemuan4/src/main/java/Array/ArrayStr.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Array;
+
+/**
+ *
+ * @author ANTON TSURAYA
+ */
+public class ArrayStr {
+    public static void main(String[] args) {
+        String mataKuliah[];
+        mataKuliah = new String[10];
+        
+        mataKuliah[0] = "Pemrograman Berbasis Objek 1";
+        mataKuliah[1] = "Visual 2";
+        mataKuliah[2] = "Rekayasa Perangkat Lunak";
+        mataKuliah[3] = "Organisasi Komputer";
+        mataKuliah[4] = "Pemrograman Web";
+
+    }
+    
+}
