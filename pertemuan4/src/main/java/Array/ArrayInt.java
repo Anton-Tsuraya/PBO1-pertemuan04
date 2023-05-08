7d51cced9990f3ff7d86cd54b52c7c0187071b9f
Anton <129922565+Anton-Tsuraya@users.noreply.github.com>
2023-05-08 19:56:38 +0800

    Create ArrayInt.java

---
+++ pertemuan4/src/main/java/Array/ArrayInt.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Array;
+
+/**
+ *
+ * @author ANTON TSURAYA
+ */
+public class ArrayInt {
+    public static void main(String[] args) {
+        int nilaiArr[] = {40,55,63,17,22,68,89,97,89};
+        System.out.println(nilaiArr.length);
+        System.out.println(nilaiArr[0]);
+        System.out.println(nilaiArr[8]);
+    }
+}
